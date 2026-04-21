1e3933f0acf8d7a717f0dcb1d5e287bfdc435ae5
Sachin Kumar <sachin.developer32@gmail.com>
2026-04-21 22:44:16 +0530

    check logging

--- app.js
+++ app.js
@@ -19,19 +19,29 @@ const corsOpts = {
   origin: config.CORS_ORIGIN || "*",
   credentials: true,
 };
+
+
+console.log("----------2nd-------------")
+
 app.use(cors(corsOpts));
 app.use(express.json({}));
 app.use(express.urlencoded({ extended: true, limit: "1gb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+console.log("----------3rd-------------")
+
 app.get("/", (req, res)=> res.send("Server is running"))
 // app.use("/", require('./src/routes/index.js'));
+
+console.log("----------4th-------------")
 
 process.on('uncaughtException', (error) => {
   console.error('Uncaught Exception:', error);
   process.exit(1);
 });
+
+console.log("----------5th-------------")
 
 module.exports = {
   app,
